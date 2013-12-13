84aee95e0f67c8bea30196c51233373a0cf06752
brooksX <brooksX@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-13 11:03:46 +0000

    added gettext to some texts without it
    git-svn-id: https://plugins.svn.wordpress.org/zanto/tags/0.1.0@820978 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/zwt-help-tabs.php
+++ views/zwt-help-tabs.php
@@ -1,14 +1,7 @@
 <?php if('tab1'==$tab['id']):?>
 
-<p>Wordpress Multisite has not been activated for your installation. to use the Zanto Wordpress Translation Plugin, you 
-			  need to activate the Multisite mode for Wordpress. You can get personal assistance in activating it <strong><a href="#">here</a></strong> or learn how to do it<strong> <a href="http://codex.wordpress.org/Create_A_Network">here</a></strong>
-       
-<br/>
-WordPress ou're running version.
-</p>
 <?php endif; ?>
 
 <?php if('tab2'==$tab['id']):?>
-<p>This is tab 2 content </p>
 <?php endif; ?>
 
