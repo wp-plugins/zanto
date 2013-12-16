7747f14b4cc477f1e5671484547fabb6a6b71828
brooksX <brooksX@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-16 13:56:34 +0000

    requirments link
    git-svn-id: https://plugins.svn.wordpress.org/zanto/tags/0.1.0@823151 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/requirements-error.php
+++ views/requirements-error.php
@@ -8,8 +8,8 @@ foreach($zwt_unfullfilled_requirments as $req=>$status){
 			if('Multisite'==$req){
             ?>
 	          <li><?php _e('<strong>Wordpress Multisite</strong> has not been activated for your installation. to use the Zanto Wordpress Translation Plugin, you 
-			  need to activate the Multisite mode for Wordpress. You can get personal assistance in activating it <strong><a href="#">here</a></strong> or learn how to do it
-			  <strong> <a href="http://codex.wordpress.org/Create_A_Network">here</a>','Zanto')?></strong>
+			  need to activate the Multisite mode for Wordpress. You can learn how to do it from
+			  <strong> <a href="http://zanto.org/?p=46">here</a>','Zanto')?></strong>
 			 </li>
             <?php
 			 }
