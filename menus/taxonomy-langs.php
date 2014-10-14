37ac9791394f4c685e3f7899a7bf960dfca7d197
brooksX <brooksX@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-14 06:13:42 +0000

    corrected php tag that caused errors in some servers. in taxonomy-langs.php
    git-svn-id: https://plugins.svn.wordpress.org/zanto/tags/0.3.2@1007070 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- menus/taxonomy-langs.php
+++ menus/taxonomy-langs.php
@@ -1,11 +1,14 @@
-
+<?php
+if ($_SERVER['SCRIPT_FILENAME'] == __FILE__)
+    die('Access denied.');
+?>
 <table class="form-table">
 		<tbody>
 		<tr class="form-field form-required">
-		    <?php if(isset($_GET['action']) && $_GET['action']='edit'):?>
-			<th scope="row" valign="top"><label for="name">Translations</label></th>
+		    <?php if(isset($_GET['action']) && $_GET['action']=='edit'):?>
+			<th scope="row" valign="top"><label for="name"><?php _e('Translations','Zanto') ?></label></th>
 			<?php else: ?>
-			<label>Translations</label>
+			<label><?php _e('Translations','Zanto') ?></label>
 			<?php endif; ?>
 			
 			<td>
@@ -23,13 +26,12 @@
 			<a href="<?php echo  add_query_arg(array('zwt_translate_tax'=>$term_id ,'source_b'=>$blog_id),  $blog_parameters[$c_id]['admin_url'].'edit-tags.php?taxonomy='.esc_html($taxonomy_name).'&post_type='. $post_type) ?>" target="_blank"
 			title="<?php printf(__('Create %s Translation','Zanto'),$b_lang);?>">  <i class="fa fa-plus-square btp-tax-icon"></i> </a>
 			<?php } elseif($edit_tax && $translated_terms[$trans_blog['blog_id']]){?>
-			<a href=" <?php echo $blog_parameters[$c_id]['admin_url'].'edit-tags.php?action=edit&taxonomy='.esc_html($taxonomy_name).'&tag_ID='.$translated_terms[$trans_blog['blog_id']].'&post_type='.$post_type.'" target="_blank" '?> 
+			<a href=" <?php echo $blog_parameters[$c_id]['admin_url'].'edit-tags.php?action=edit&taxonomy='.esc_html($taxonomy_name).'&tag_ID='.$translated_terms[$trans_blog['blog_id']].'&post_type='.$post_type ?>" target="_blank"
 			title="<?php printf(__('Edit %s Translation','Zanto'),$b_lang);?>"> <i class="fa fa-check-square-o btp-tax-icon"></i></a>
 			<?php } ?>
 			</p>
-			 <? } ?>
+			 <?php } ?>
 			<p class="description"><?php _e('The chosen category should be the translated version of this category.','Zanto')?></p>
 			</td>
 		</tr>
 </tbody></table>
-<?
